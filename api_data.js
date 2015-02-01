7fd58a1042f1325176e32047bc7e7afdeb5549ff
Romerio <romerio.unix@gmail.com>
2015-02-01 10:51:08 -0300

    Alteracoes 4

--- api_data.js
+++ api_data.js
@@ -82,7 +82,7 @@ define({ "api": [
   {
     "type": "GET",
     "url": "http://hostname/chats/messages",
-    "title": "Show all messages headers",
+    "title": "Show messages headers",
     "name": "ShowMessages",
     "group": "Chat_Messages",
     "header": {
@@ -123,7 +123,7 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "language",
-            "description": "<p>Langhage that I can talk at the chat.</p> "
+            "description": "<p>Language that I can talk at the chat.</p> "
           }
         ]
       }
@@ -206,6 +206,170 @@ define({ "api": [
     "groupTitle": "Chats"
   },
   {
+    "type": "GET",
+    "url": "http://hostname/friends",
+    "title": "Get a user friend list basic profile",
+    "name": "GetFriends",
+    "group": "Profiles",
+    "header": {
+      "fields": {
+        "Headers": [
+          {
+            "group": "Headers",
+            "type": "String",
+            "optional": false,
+            "field": "X-Token",
+            "description": "<p>Token of session</p> "
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "friend",
+            "description": "<p>List of friends basic profile.</p> "
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "    HTTP/1.1 200 OK\n    {\n      \"friends\": [\n\t\t\t\t\t\t{\"nick\": \"Anna\", \"photo\": \"http://media.chatapp.com/photo/fsdfrfsdfsdf\"},\n\t\t\t\t\t\t{\"nick\": \"Giselle\", \"photo\": \"http://media.chatapp.com/photo/fsdfrfsiuaof\"},\n\t\t\t\t\t\t{\"nick\": \"Mike\", \"photo\": \"http://media.chatapp.com/photo/fsdfaosifsdf\"},\n\t\t\t\t\t]\n    }",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "./example.js",
+    "groupTitle": "Profiles"
+  },
+  {
+    "type": "GET",
+    "url": "http://hostname/profiles/:nick",
+    "title": "Get a user profile",
+    "name": "GetProfile",
+    "group": "Profiles",
+    "header": {
+      "fields": {
+        "Headers": [
+          {
+            "group": "Headers",
+            "type": "String",
+            "optional": false,
+            "field": "X-Token",
+            "description": "<p>Token of session</p> "
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "nick",
+            "description": "<p>User nickname.</p> "
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "photo",
+            "description": "<p>Link for user photo.</p> "
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "friends",
+            "description": "<p>Number of user friends.</p> "
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "rate",
+            "description": "<p>Rate of a user at the SAT.</p> "
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "    HTTP/1.1 200 OK\n    {\n      \"nick\": \"Anna\",\n      \"photo\": \"http://media.chatapp.com/photo/fsdfrfsdfsdf\",\n\t\t \"friends\": 32,\n\t\t \"rate\": \"Cool girl\",\n    }",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "./example.js",
+    "groupTitle": "Profiles"
+  },
+  {
+    "type": "POST",
+    "url": "http://hostname/profiles",
+    "title": "Update a user profile",
+    "name": "UpdateProfile",
+    "group": "Profiles",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "nick",
+            "description": "<p>User nickname.</p> "
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "about",
+            "description": "<p>User description.</p> "
+          }
+        ]
+      }
+    },
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "{\n  \"nick\": \"Mero\",\n  \"about\": \"I'm a nice man\"\n}",
+        "type": "json"
+      }
+    ],
+    "header": {
+      "fields": {
+        "Headers": [
+          {
+            "group": "Headers",
+            "type": "String",
+            "optional": false,
+            "field": "Content-type",
+            "description": "<p>application/json</p> "
+          },
+          {
+            "group": "Headers",
+            "type": "String",
+            "optional": false,
+            "field": "X-Token",
+            "description": "<p>Token of session</p> "
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "./example.js",
+    "groupTitle": "Profiles"
+  },
+  {
     "type": "POST",
     "url": "http://hostname/auth",
     "title": "Login user",
@@ -226,7 +390,7 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "password",
-            "description": "<p>user password.</p> "
+            "description": "<p>User password.</p> "
           }
         ]
       }
